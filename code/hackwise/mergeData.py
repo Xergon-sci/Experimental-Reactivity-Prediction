abf90221287f4ae627f2e6b27ab3f7238aa28c72
M.Jacobs <jacobs.michiel@icloud.com>
2021-03-07 10:11:55 +0100

    Added full dataset

--- code/hackwise/mergeData.py
+++ code/hackwise/mergeData.py
@@ -8,6 +8,7 @@ sys.path.append(UTIL_PATH)
 
 import pandas as pd
 from data_utility import loaddata
+import numpy as np
 
 baseData = pd.read_csv(r'C:\Users\Michiel Jacobs\Research\Master Thesis\Experimental-Reactivity-Prediction\data\CNOS_sub1_10to20_10k\backup\CNOS_sub1_10to20_10k.csv')
 resultData = loaddata(r'C:\Users\Michiel Jacobs\Research\Master Thesis\Experimental-Reactivity-Prediction\data\CNOS_sub1_10to20_10k\backup')
@@ -33,4 +34,8 @@ result = result.drop(['jid_x','jid_y'], axis = 1)
 result = result[['jid','cid', 'smiles', 'carbon', 'fluorine', 'chlorine', 'bromine', 'iodine', 'sulfur', 'phosphorous', 'acyclic nitrogen', 'cyclic nitrogen', 'acyclic oxygen', 'cyclic oxygen', 
 'heavy atoms', 'acyclic single bonds', 'acyclic double bonds', 'acyclic triple bonds', 'cyclic single bonds', 'cyclic double bonds', 'cyclic triple bonds', 'rotable bonds', 'h-bond acceptor sites', 'h-bond acceptor atoms', 'h-bond donor sites', 'h-bond donor atoms', 'negative charges', 'positive charges', 'acyclic single valent nodes', 'acyclic divalent nodes', 'acyclic trivalent nodes', 'acyclic tetravalent nodes', 'cyclic divalent nodes', 'cyclic trivalent nodes', 'cyclic tetravalent nodes', '3-membered rings', '4-membered rings', '5-membered rings', '6-membered rings', '7-membered rings', '8-membered rings', '9-membered rings', '>= 10-membered rings', 'nodes shared by >= 2 rings', 'edges shared by >= 2 rings', 'functional groups', 'optimized_geometry', 'self_consistent_field', 'zero_point_energy', 'thermal_correction_to_energy', 'thermal_correction_to_enthalpy', 'thermal_correction_to_gibbs_free_energy', 'homo', 'lumo', 'natural_population_analysis', 'coulomb_matrix']]
 
-result.to_json('test.json', orient='index', indent=2)
+# save each part of df with a max memory size of less then 100 MB
+sections = np.array_split(result, 10)
+
+for i,s in enumerate(sections):
+    s.to_json(r'C:\Users\Michiel Jacobs\Research\Master Thesis\Experimental-Reactivity-Prediction\data\CNOS_sub1_10to20_10k\Full\CNOS_sub1_10to20_10k_{}'.format(i), orient='index', indent=2)
